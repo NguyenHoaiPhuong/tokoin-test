26f87ef6ef4810245404fe70980c383d535cea39
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2019-12-08 16:31:25 +0700

    [model] add user, ticket and organization struct

---
+++ golang/model/user.go
@@ -0,0 +1,27 @@
+package model
+
+// User struct
+type User struct {
+	ID             int      `json:"_id"`
+	URL            string   `json:"url"`
+	ExternalID     string   `json:"external_id"`
+	Name           string   `json:"name"`
+	Alias          string   `json:"alias"`
+	CreatedAt      string   `json:"created_at"`
+	Active         bool     `json:"active"`
+	Verified       bool     `json:"verified"`
+	Shared         bool     `json:"shared"`
+	Locale         string   `json:"locale"`
+	Timezone       string   `json:"timezone"`
+	LastLoginAt    string   `json:"last_login_at"`
+	Email          string   `json:"email"`
+	Phone          string   `json:"phone"`
+	Signature      string   `json:"signature"`
+	OrganizationID int      `json:"organization_id"`
+	Tags           []string `json:"tags"`
+	Suspended      bool     `json:"suspended"`
+	Role           string   `json:"role"`
+}
+
+// Users : slice of users
+type Users []*User
